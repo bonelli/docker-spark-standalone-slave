a0bab6507484e28e3a013e0ced4965160037b6bf
Federico Bonelli <federico.bonelli@moxoff.com>
2014-11-11 22:07:21 +0100

    dynamic hostname

--- run-docker.sh
+++ run-docker.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -e "SPARK_PUBLIC_DNS=aede" -p 7077:7077 -p 8080:8080 bonelli/spark-standalone-master
+sudo docker run -d -e "SPARK_PUBLIC_DNS=$(hostname)" -p 7077:7077 -p 8080:8080 bonelli/spark-standalone-master
