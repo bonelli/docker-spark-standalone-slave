1e2e77ab8f77b02b974c391ea8454673637a208e
Federico Bonelli <federico.bonelli@moxoff.com>
2014-11-15 23:26:14 +0100

    added the volumes for mounting /home, so we can use shared NFS homes

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 MASTER_URL="spark://aede:7077"
-sudo docker run -d --net=host -e "MASTER_URL=${MASTER_URL}" bonelli/spark-standalone-slave
+sudo docker run -d --net=host -v /home:/home -e "MASTER_URL=${MASTER_URL}" bonelli/spark-standalone-slave
--- run-shell-docker.sh
+++ run-shell-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 MASTER_URL=spark://aede:7077
-sudo docker run -ti --rm --net=host -e MASTER_URL=${MASTER_URL} bonelli/spark-standalone-slave /bin/bash ./bin/spark-shell --master ${MASTER_URL}
+sudo docker run -ti --rm --net=host -v /home:/home -e MASTER_URL=${MASTER_URL} bonelli/spark-standalone-slave /bin/bash ./bin/spark-shell --master ${MASTER_URL}
